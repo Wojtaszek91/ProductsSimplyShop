967644bb64782f70b4c1d61ebf96d573214ee4e0
Wojtaszek91 <spiochtar@o2.pl>
2020-03-19 15:04:06 +0100

    Add AboutMeModel addnotations

--- SimplyProductShop/SimplyProductShop/Models/AboutMeModel.cs
+++ SimplyProductShop/SimplyProductShop/Models/AboutMeModel.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimplyProductShop.Models
 {
     public class AboutMeModel
     {
+        [Required]
+        string WhoIam { get; set; }
+        [Required]
+        string WhatImIdoing { get; set; }
+        string MyExperience { get; set; }
     }
 }
