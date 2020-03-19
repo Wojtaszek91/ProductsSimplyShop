9acd2ed6c4c16691637433ca6cc459321a404985
Wojtaszek91 <spiochtar@o2.pl>
2020-03-19 17:53:57 +0100

    add product View

--- SimplyProductShop/SimplyProductShop/Controllers/ProductsController.cs
+++ SimplyProductShop/SimplyProductShop/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimplyProductShop.Models;
+using SimplyProductShop.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace SimplyProductShop.Controllers
 {
     public class ProductsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Products
         public ActionResult Index()
         {
-            return View();
+            var productList = _context.ProductsModel.ToList(); 
+            return View(new ProductListViewModel { ProductList = productList});
         }
     }
 }
