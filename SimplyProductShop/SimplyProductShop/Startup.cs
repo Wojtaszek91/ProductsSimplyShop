35249061cd24de731ab639a50531fd62220f8f6e
Wojtaszek91 <spiochtar@o2.pl>
2020-03-19 13:00:48 +0100

    initial commint
    Created ASP.NET MVC project with Individual users Authentication

---
+++ SimplyProductShop/SimplyProductShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimplyProductShop.Startup))]
+namespace SimplyProductShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
