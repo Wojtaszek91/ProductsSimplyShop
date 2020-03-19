a776a2082a03e838607bb9dbef1af029dfe47780
Wojtaszek91 <spiochtar@o2.pl>
2020-03-19 13:58:52 +0100

    add prodcut model

---
+++ SimplyProductShop/SimplyProductShop/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimplyProductShop.Models
+{
+    public class Products
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Img { get; set; }
+        bool IsAvaliable { get; set; }
+    }
+}
