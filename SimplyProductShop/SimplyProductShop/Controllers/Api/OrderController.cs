2956a235e14ad7da128172f66f14c6168db1cb10
Wojtaszek91 <spiochtar@o2.pl>
2020-03-27 18:31:29 +0100

    add check if edge cases into MakeOrder Action in api

--- SimplyProductShop/SimplyProductShop/Controllers/Api/OrderController.cs
+++ SimplyProductShop/SimplyProductShop/Controllers/Api/OrderController.cs
@@ -18,25 +18,36 @@ namespace SimplyProductShop.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
+        // Checking edges cases and making an order in Db or send back bad request with message what happend.
         [HttpPost]
         public IHttpActionResult MakeOrder(OrderDto order)
         {
+            if (order.ProductsIdList.Count() == 0)
+                return BadRequest("No products found.");
+            if (order.CustomerId == 0)
+                return BadRequest("No customer found.");
 
             var productList = _context.ProductsModel.Where(p => order.ProductsIdList.Contains(p.Id)).ToList();
+
+            if (productList.Count() != order.ProductsIdList.Count())
+                return BadRequest("Not all products has been found.");
+
             foreach (var product in productList) {
-                _context.OrderModels.Add(new OrderModel()
+                if (product.IsAvaliable == false)
+                    return BadRequest("Product is not availabel !");
+
+                var orderForDb = new OrderModel()
                 {
                     customer = _context.Customers.Single(c => c.Id == order.CustomerId),
                     product = product,
                     orderDate = DateTime.Now
-                });
+                };
+                _context.OrderModels.Add(orderForDb);
             }
 
             _context.SaveChanges();
             
             return Ok();
         }
-
-
     }
 }
