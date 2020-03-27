477df49b824c30ec43c8c133e51305043dde5590
Wojtaszek91 <spiochtar@o2.pl>
2020-03-27 13:20:52 +0100

    add feathers poperties into OrderModel

--- SimplyProductShop/SimplyProductShop/Models/OrderModel.cs
+++ SimplyProductShop/SimplyProductShop/Models/OrderModel.cs
@@ -9,8 +9,14 @@ namespace SimplyProductShop.Models
     public class OrderModel
     {
         [Required]
-        public DateTime orderDate;
-        public bool isPayed;
-        public bool isDelivered;
+        public int Id { get; set; }
+        public Customer customer { get; set; }
+        public Product product { get; set; }
+        [Required]
+        public DateTime orderDate { get; set; }
+        public DateTime deliveredDate { get; set; }
+        public DateTime payedDate { get; set; }
+        public bool isPayed { get; set; }
+        public bool isDelivered { get; set; }
     }
 }
